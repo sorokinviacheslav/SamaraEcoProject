6ce32e3a20ba176aac07a39da065232b381a71e7
sorokinviacheslav <alpha137@yandex.ru>
2020-02-11 16:57:49 +0400

    main

---
+++ modules/web/src/com/magenta/samara/eco/mentionjs/MentionJsState.java
@@ -0,0 +1,10 @@
+package com.magenta.samara.eco.mentionjs;
+
+import com.vaadin.shared.JavaScriptExtensionState;
+
+import java.util.List;
+
+public class MentionJsState extends JavaScriptExtensionState {
+
+    public List<String> logins;
+}
--- modules/web/src/com/magenta/samara/eco/web/screens/organization/OrganizationBrowse.java
+++ modules/web/src/com/magenta/samara/eco/web/screens/organization/OrganizationBrowse.java
@@ -1,11 +1,41 @@
 package com.magenta.samara.eco.web.screens.organization;
 
+import com.haulmont.cuba.gui.components.TextArea;
+import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.screen.*;
 import com.magenta.samara.eco.entity.Organization;
+import com.magenta.samara.eco.mentionjs.MentionJsExtension;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 @UiController("eco_Organization.browse")
 @UiDescriptor("organization-browse.xml")
 @LookupComponent("organizationsTable")
 @LoadDataBeforeShow
 public class OrganizationBrowse extends StandardLookup<Organization> {
+
+    @Inject
+    private TextArea<String> testTextArea;
+    @Inject
+    private CollectionContainer<Organization> organizationsDc;
+
+    @Subscribe
+    public void onAfterShow(AfterShowEvent event) {
+        com.vaadin.ui.TextArea vTextArea = testTextArea.unwrap(com.vaadin.ui.TextArea.class);
+        // enable extension
+        MentionJsExtension extension = new MentionJsExtension(vTextArea);
+        List<String> list=new ArrayList<>();
+        for(Organization o:organizationsDc.getItems()) {
+            list.add(o.getName());
+        }
+        extension.setUsers(list);
+    }
+
+
+
+
+
+    
 }
