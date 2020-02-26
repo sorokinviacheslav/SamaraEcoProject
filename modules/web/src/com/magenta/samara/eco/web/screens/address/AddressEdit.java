32f53ffd2c5874d4711b4287b1f4623c2f597e63
sorokinviacheslav <alpha137@yandex.ru>
2020-02-26 13:57:45 +0400

    main

--- modules/web/src/com/magenta/samara/eco/web/screens/address/AddressEdit.java
+++ modules/web/src/com/magenta/samara/eco/web/screens/address/AddressEdit.java
@@ -2,6 +2,7 @@ package com.magenta.samara.eco.web.screens.address;
 
 import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.gui.Notifications;
 import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.screen.*;
@@ -20,6 +21,8 @@ public class AddressEdit extends StandardEditor<Address> {
     @Inject
     private Form streetForm;
     @Inject
+    private Form form;
+    @Inject
     private LookupField<StreetType> streetType;
     @Inject
     private LookupPickerField<Street> streetField;
@@ -29,6 +32,12 @@ public class AddressEdit extends StandardEditor<Address> {
     private TextField<String> streetName;
     @Inject
     private DataManager dataManager;
+    @Inject
+    private ScreenValidation screenValidation;
+    @Inject
+    private Notifications notifications;
+    @Inject
+    private TabSheet tabSheet;
 
     @Subscribe("streetField")
     public void onStreetFieldValueChange(HasValue.ValueChangeEvent<Street> event) {
@@ -45,6 +54,34 @@ public class AddressEdit extends StandardEditor<Address> {
         }
         event.resume();
     }
+
+    @Subscribe("nextButton")
+    public void onNextButtonClick(Button.ClickEvent event) {
+        ValidationErrors errors = screenValidation.validateUiComponents(form.getComponents());
+        if(errors.isEmpty()) {
+            TabSheet.Tab tab = tabSheet.getTab("additionalTab");
+            TabSheet.Tab tabMain = tabSheet.getTab("mainTab");
+            tabMain.setEnabled(false);
+            tab.setEnabled(true);
+            tabSheet.setSelectedTab(tab);
+            return;
+        }
+        for (ValidationErrors.Item er : errors.getAll()) {
+            notifications.create(Notifications.NotificationType.TRAY)
+                    .withCaption(er.description)
+                    .show();
+        }
+    }
+
+    @Subscribe("backButton")
+    public void onBackButtonClick(Button.ClickEvent event) {
+        TabSheet.Tab tabAd = tabSheet.getTab("additionalTab");
+        TabSheet.Tab tabMain = tabSheet.getTab("mainTab");
+        tabAd.setEnabled(false);
+        tabMain.setEnabled(true);
+        tabSheet.setSelectedTab(tabMain);
+    }
+
 
 
 }
